4b0f621e2ad6227c7586d3f012fce153e8126272
Fithi Teklom <fthiteklom@gmail.com>
2025-11-08 12:46:50 +0000

    refactored it in 2 files

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -3,24 +3,15 @@ function increment(node) {
   let current = node.textContent;
   node.textContent = Number(current) + 1;
 }
+import {Header} from "./header.js";
+import {Main} from "./main.js";
 
 export function App() {
   const body = document.createElement("body");
-
-  const header = document.createElement("header");
-  header.innerHTML = `
-        <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
-    `;
+  const header = Header();
+  const main = Main();
   body.appendChild(header);
-
-  const main = document.createElement("main");
-  main.innerHTML = `
-        <p id="counter" data-testid="counter">0</p>
-        <button id="increment">Increment</button>
-    `;
   body.appendChild(main);
-
   const button = body.querySelector("#increment");
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
@@ -29,3 +20,12 @@ export function App() {
 
   return body;
 }
+//body.innerHTML = `
+    // <header>
+    //   <h1>Number Counter</h1>
+    //   <p>A simple counter. Press increment to increase the count by one.</p>
+    // </header>
+    // <main>
+    //   <p id="counter" data-testid="counter">0</p>
+    //   <button id="increment">Increment</button>
+    // </main>
